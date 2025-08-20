888a7129ee65e92f602933d88110f9e80c2f0e8e
Wangariiisharon <mwangiiisharon@gmail.com>
2025-08-20 09:33:24 +0300

    Updated sanity env

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from "@sanity/client";
 
 export const client = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: "2022-03-07",
   useCdn: false,
   token: process.env.SANITY_API_WRITE_TOKEN, //ensure token has write permissions
